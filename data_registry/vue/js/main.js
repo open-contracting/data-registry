d30a6040d27b790d9ab503c54c15555bdba745a3
Kuba Krafka <jakub@krafka.net>
2021-02-23 12:26:59 +0100

    added vue skeleton

---
+++ data_registry/vue/js/main.js
@@ -0,0 +1,3 @@
+import "babel-polyfill";
+var Promise = require('es6-promise').Promise;
+require('es6-promise').polyfill();
---
+++ data_registry/vue/webpack.production.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = merge(common, {
+    mode: 'production',
+    devtool: 'source-map',
+    plugins: [
+        new UglifyJSPlugin({
+            sourceMap: true
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
+            }
+        ]
+    }
+});
