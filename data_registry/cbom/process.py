eb45ec77798000face161f5dd5e137eea2cde824
Tomas Mrazek <t.mrazek@email.cz>
2021-05-05 13:57:12 +0200

    cbom refactoring

--- data_registry/cbom/process.py
+++ data_registry/cbom/process.py
@@ -1,18 +1,87 @@
 import logging
+from datetime import datetime
 
-from data_registry.models import Job
+from django.db.models.query_utils import Q
+
+from data_registry.cbom.tasks import TaskFactory
+from data_registry.models import Job, Task
 
 logger = logging.getLogger('cbom')
 
 
 def process(collection):
     logger.debug("Processing collection {}".format(collection))
-    jobs = collection.jobs
+
+    if should_be_planned(collection):
+        plan(collection)
+
+    jobs = Job.objects.filter(collection=collection).filter(~Q(status="COMPLETED"))
+
+    for job in jobs:
+        tasks = Task.objects.filter(job=job).order_by("order")
+        run_next_planned = True
+        job_complete = True
+
+        for task in tasks:
+            # finished task -> no action needed
+            if task.status == "COMPLETED":
+                continue
+
+            _task = TaskFactory.get_task(job, task)
+
+            try:
+                if task.status in ["WAITING", "RUNNING"]:
+                    if _task.get_status() in ["WAITING", "RUNNING"]:
+                        run_next_planned = False
+                        continue
+                    else:
+                        task.end = datetime.now()
+                        task.status = "COMPLETED"
+                        task.result = "OK"
+                elif task.status == "PLANNED" and run_next_planned:
+                    if job.status == "PLANNED":
+                        job.start = datetime.now()
+                        job.status = "RUNNNIG"
+                        job.save()
+
+                    # run task
+                    _task.run()
+                    task.start = datetime.now()
+                    task.status = "RUNNING"
+                    run_next_planned = False
+
+                task.save()
+                job_complete &= task.status == "COMPLETED"
+            except Exception as e:
+                job_complete = True
+                task.end = datetime.now()
+                task.status = "COMPLETED"
+                task.result = "FAILED"
+                task.note = str(e)
+                task.save()
+                break
+
+        if job_complete:
+            job.status = "COMPLETED"
+            job.end = datetime.now()
+            job.save()
 
 
 def should_be_planned(collection):
-    jobs = Job.objects.filter(collection = collection)
+    jobs = Job.objects.filter(collection=collection)
     if not jobs:
         return True
     else:
         return False
+
+
+def plan(collection):
+    job = collection.job.create(
+        start=datetime.now(),
+        status="PLANNED"
+    )
+
+    tasks = ["scrape"]
+
+    for i, t in enumerate(tasks, start=1):
+        job.task.create(status="PLANNED", type=t, order=i)
--- data_registry/management/commands/cbom.py
+++ data_registry/management/commands/cbom.py
@@ -1,11 +1,9 @@
 import logging
-from datetime import datetime
 
 from django.core.management.base import BaseCommand
-from django.db.models.query_utils import Q
 
-from data_registry.management.commands.tasks import TaskFactory
-from data_registry.models import Collection, Job, Task
+from data_registry.cbom.process import process
+from data_registry.models import Collection
 
 logger = logging.getLogger('cbom')
 
@@ -18,65 +16,4 @@ class Command(BaseCommand):
         logger.info("CBOM started")
 
         for collection in Collection.objects.all():
-            jobs = Job.objects.filter(collection=collection).filter(~Q(status="COMPLETED"))
-
-            # CREATE NEW JOB
-            if not jobs:
-                job = collection.job.create(
-                    start=datetime.now(),
-                    status="PLANNED"
-                )
-
-                job.task.create(status="PLANNED", type="test", order=1)
-                job.task.create(status="PLANNED", type="test", order=2)
-
-                jobs = [job]
-
-            for job in jobs:
-                tasks = Task.objects.filter(job=job).order_by("order")
-                run_next_planned = True
-                job_complete = True
-
-                for task in tasks:
-                    # finished task -> no action needed
-                    if task.status == "COMPLETED":
-                        continue
-
-                    _task = TaskFactory.get_task(job, task)
-
-                    try:
-                        if task.status in ["WAITING", "RUNNING"]:
-                            if _task.get_status() in ["WAITING", "RUNNING"]:
-                                run_next_planned = False
-                                continue
-                            else:
-                                task.end = datetime.now()
-                                task.status = "COMPLETED"
-                                task.result = "OK"
-                        elif task.status == "PLANNED" and run_next_planned:
-                            if job.status == "PLANNED":
-                                job.start = datetime.now()
-                                job.status = "RUNNNIG"
-                                job.save()
-
-                            # run task
-                            _task.run()
-                            task.start = datetime.now()
-                            task.status = "RUNNING"
-                            run_next_planned = False
-
-                        task.save()
-                        job_complete &= task.status == "COMPLETED"
-                    except Exception as e:
-                        job_complete = True
-                        task.end = datetime.now()
-                        task.status = "COMPLETED"
-                        task.result = "FAILED"
-                        task.note = str(e)
-                        task.save()
-                        break
-
-                if job_complete:
-                    job.status = "COMPLETED"
-                    job.end = datetime.now()
-                    job.save()
+            process(collection)
