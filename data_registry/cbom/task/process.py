a37f5fa66338baa649a91d665e8f5907b1eec226
Tomas Mrazek <t.mrazek@email.cz>
2021-05-17 13:45:00 +0200

    process task get_status implemented

--- data_registry/cbom/task/process.py
+++ data_registry/cbom/task/process.py
@@ -1,6 +1,8 @@
 import re
 
 import requests
+from django.conf import settings
+from requests.models import HTTPError
 
 from data_registry.cbom.task.task import BaseTask
 from data_registry.models import Task
@@ -27,7 +29,17 @@ class Process(BaseTask):
         if not self.process_id:
             return None
         else:
-            return Task.Status.RUNNING
+            try:
+                resp = requests.get(f"{settings.PROCESS_HOST}api/v1/tree/{self.process_id}/")
+                resp.raise_for_status()
+            except HTTPError as e:
+                raise Exception(f"Unable to get status of process #{self.process_id}") from e
+
+            json = resp.json()
+
+            is_last_completed = json[-1].get("completed_at", None) is not None
+
+            return Task.Status.COMPLETED if is_last_completed else Task.Status.RUNNING
 
     def get_process_id(self):
         # process id is published in scrapy log
