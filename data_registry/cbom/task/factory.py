d462d4c1f801ad95192a3ff4c864f9fa59453a01
Tomas Mrazek <t.mrazek@email.cz>
2021-05-25 08:22:08 +0200

    task factory fix

--- data_registry/cbom/task/factory.py
+++ data_registry/cbom/task/factory.py
@@ -1,4 +1,5 @@
 
+from data_registry.cbom.task.exporter import Exporter
 from data_registry.cbom.task.pelican import Pelican
 from data_registry.cbom.task.process import Process
 from data_registry.cbom.task.scrape import Scrape
@@ -15,5 +16,7 @@ class TaskFactory:
             return Process(job)
         elif type == "pelican":
             return Pelican(job)
+        elif type == "exporter":
+            return Exporter(job)
         else:
             raise Exception("Unsupported task type")
