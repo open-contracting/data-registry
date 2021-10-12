0509224bf3981f2ae45dbed1d9676c51f7e18fc6
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-10-12 17:02:16 -0400

    fix: Fix regression in 40378976 causing "$attrs is readonly" and "$listeners is readonly"

--- data_registry/vue/webpack.common.js
+++ data_registry/vue/webpack.common.js
@@ -35,7 +35,7 @@ module.exports = {
   resolve: {
     alias: {
       // If using the runtime only build
-      vue$: "vue/dist/vue.js", // 'vue/dist/vue.runtime.common.js' for webpack 1
+      vue$: "vue/dist/vue.min.js", // 'vue/dist/vue.runtime.common.js' for webpack 1
       // Or if using full build of Vue (runtime + compiler)
       // vue$: 'vue/dist/vue.esm.js'      // 'vue/dist/vue.common.js' for webpack 1
     },
