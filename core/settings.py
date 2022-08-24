64e790cba6876dfe81d0e2fd844e13b5a36adc87
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-08-23 23:15:00 -0400

    chore: Eliminate unused STATIC_VERSION environment variable

--- core/settings.py
+++ core/settings.py
@@ -139,12 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-if production:
-    STATIC_VERSION = os.getenv("STATIC_VERSION")
-    STATIC_URL = f"/static/{STATIC_VERSION}/"
-else:
-    STATIC_URL = "/static/"
-
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
