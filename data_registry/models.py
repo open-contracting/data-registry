97c8a4d983a567815e3961395c9ccccf7fb35e63
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-09-16 15:04:06 -0400

    chore(style): Don't do individual imports from django.db.models in models.py

--- data_registry/models.py
+++ data_registry/models.py
@@ -1,36 +1,24 @@
-from django.db.models import (
-    CASCADE,
-    BooleanField,
-    DateField,
-    DateTimeField,
-    Exists,
-    ForeignKey,
-    IntegerField,
-    JSONField,
-    Manager,
-    Model,
-    OuterRef,
-    TextChoices,
-    TextField,
-)
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 from markdownx.models import MarkdownxField
 
 
-class Job(Model):
-    collection = ForeignKey("Collection", related_name="job", on_delete=CASCADE, verbose_name="publication")
-    start = DateTimeField(blank=True, null=True, db_index=True, verbose_name="job started at")
-    end = DateTimeField(blank=True, null=True, db_index=True, verbose_name="job ended at")
+class Job(models.Model):
+    collection = models.ForeignKey(
+        "Collection", related_name="job", on_delete=models.CASCADE, verbose_name="publication"
+    )
+    start = models.DateTimeField(blank=True, null=True, db_index=True, verbose_name="job started at")
+    end = models.DateTimeField(blank=True, null=True, db_index=True, verbose_name="job ended at")
 
-    class Status(TextChoices):
+    class Status(models.TextChoices):
         WAITING = "WAITING", "WAITING"
         PLANNED = "PLANNED", "PLANNED"
         RUNNING = "RUNNING", "RUNNING"
         COMPLETED = "COMPLETED", "COMPLETED"
 
-    status = TextField(choices=Status.choices, blank=True)
+    status = models.TextField(choices=Status.choices, blank=True)
 
-    context = JSONField(
+    context = models.JSONField(
         blank=True,
         default=dict,
         help_text="<dl>"
@@ -53,17 +41,17 @@ class Job(Model):
         "</dl>",
     )
 
-    active = BooleanField(
+    active = models.BooleanField(
         default=False, help_text="Set this as the active job for the publication from the publication's page."
     )
 
-    archived = BooleanField(
+    archived = models.BooleanField(
         default=False,
         verbose_name="temporary data deleted",
         help_text="Whether the temporary data created by job tasks has been deleted.",
     )
 
-    keep_all_data = BooleanField(
+    keep_all_data = models.BooleanField(
         default=False,
         verbose_name="preserve temporary data",
         help_text="By default, temporary data created by job tasks is deleted after the job "
@@ -72,28 +60,28 @@ class Job(Model):
         'when ready, uncheck this box and run the "manageprocess" management command.',
     )
 
-    tenders_count = IntegerField(default=0)
-    tenderers_count = IntegerField(default=0)
-    tenders_items_count = IntegerField(default=0)
-    parties_count = IntegerField(default=0)
-    awards_count = IntegerField(default=0)
-    awards_items_count = IntegerField(default=0)
-    awards_suppliers_count = IntegerField(default=0)
-    contracts_count = IntegerField(default=0)
-    contracts_items_count = IntegerField(default=0)
-    contracts_transactions_count = IntegerField(default=0)
-    documents_count = IntegerField(default=0)
-    plannings_count = IntegerField(default=0)
-    milestones_count = IntegerField(default=0)
-    amendments_count = IntegerField(default=0)
+    tenders_count = models.IntegerField(default=0)
+    tenderers_count = models.IntegerField(default=0)
+    tenders_items_count = models.IntegerField(default=0)
+    parties_count = models.IntegerField(default=0)
+    awards_count = models.IntegerField(default=0)
+    awards_items_count = models.IntegerField(default=0)
+    awards_suppliers_count = models.IntegerField(default=0)
+    contracts_count = models.IntegerField(default=0)
+    contracts_items_count = models.IntegerField(default=0)
+    contracts_transactions_count = models.IntegerField(default=0)
+    documents_count = models.IntegerField(default=0)
+    plannings_count = models.IntegerField(default=0)
+    milestones_count = models.IntegerField(default=0)
+    amendments_count = models.IntegerField(default=0)
 
-    date_from = DateField(blank=True, null=True, verbose_name="minimum release date")
-    date_to = DateField(blank=True, null=True, verbose_name="maximum release date")
-    ocid_prefix = TextField(blank=True, verbose_name="OCID prefix")
-    license = TextField(blank=True)
+    date_from = models.DateField(blank=True, null=True, verbose_name="minimum release date")
+    date_to = models.DateField(blank=True, null=True, verbose_name="maximum release date")
+    ocid_prefix = models.TextField(blank=True, verbose_name="OCID prefix")
+    license = models.TextField(blank=True)
 
-    created = DateTimeField(auto_now_add=True, blank=True, null=True, db_index=True)
-    modified = DateTimeField(auto_now=True, blank=True, null=True, db_index=True)
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True, db_index=True)
+    modified = models.DateTimeField(auto_now=True, blank=True, null=True, db_index=True)
 
     def __str__(self):
         return f"{self.format_datetime(self.start)} .. {self.format_datetime(self.end)} ({self.id})"
@@ -102,27 +90,29 @@ class Job(Model):
         return dt.strftime("%d-%b-%y") if dt else ""
 
 
-class PublicCollectionManager(Manager):
+class PublicCollectionManager(models.Manager):
     def get_queryset(self):
         # https://docs.djangoproject.com/en/3.2/ref/models/expressions/#some-examples
-        active_jobs = Job.objects.filter(collection=OuterRef("pk"), active=True)
-        return super().get_queryset().filter(Exists(active_jobs), public=True)
+        active_jobs = Job.objects.filter(collection=models.OuterRef("pk"), active=True)
+        return super().get_queryset().filter(models.Exists(active_jobs), public=True)
 
 
-class Collection(Model):
+class Collection(models.Model):
     class Meta:
         verbose_name = "publication"
 
-    title = TextField(
+    title = models.TextField(
         help_text='The name of the publication, following the <a href="https://docs.google.com/document/d/'
         '14ZXlAB6GWeK4xwDUt9HGi0fTew4BahjZQ2owdLLVp6I/edit#heading=h.t81hzvffylry">naming conventions</a>, '
         "and omitting the country name."
     )
 
-    country = TextField(blank=True, help_text="The official name of the country from which the data originates.")
-    country_flag = TextField(blank=True)
+    country = models.TextField(
+        blank=True, help_text="The official name of the country from which the data originates."
+    )
+    country_flag = models.TextField(blank=True)
 
-    language = TextField(blank=True, help_text='The languages used within data fields: for example, "Spanish".')
+    language = models.TextField(blank=True, help_text='The languages used within data fields: for example, "Spanish".')
     description = MarkdownxField(
         blank=True,
         help_text="The first paragraph of the description of the publication, as Markdown text, following the <a "
@@ -134,37 +124,37 @@ class Collection(Model):
         help_text="The remaining paragraphs of the description of the publication, as "
         'Markdown text, which will appear under "Show more".',
     )
-    last_update = DateField(
+    last_update = models.DateField(
         blank=True,
         null=True,
         verbose_name="last updated",
         help_text="The date on which the most recent 'collect' job task completed.",
     )
 
-    license_custom = ForeignKey(
+    license_custom = models.ForeignKey(
         "License",
         related_name="collection",
-        on_delete=CASCADE,
+        on_delete=models.CASCADE,
         blank=True,
         null=True,
         verbose_name="data license",
         help_text="If not set, the Overview section will display " "the license URL within the OCDS package.",
     )
 
-    source_id = TextField(
+    source_id = models.TextField(
         verbose_name="source ID",
         help_text="The name of the spider in Kingfisher Collect. If a new spider is not listed, "
         "Kingfisher Collect needs to be re-deployed to the registry's server.",
     )
 
-    source_url = TextField(blank=True, verbose_name="source URL", help_text="The URL of the publication.")
+    source_url = models.TextField(blank=True, verbose_name="source URL", help_text="The URL of the publication.")
 
-    class Frequency(TextChoices):
+    class Frequency(models.TextChoices):
         MONTHLY = "MONTHLY", _("Monthly")
         HALF_YEARLY = "HALF_YEARLY", _("Every 6 months")
         ANNUALLY = "ANNUALLY", _("Annually")
 
-    update_frequency = TextField(
+    update_frequency = models.TextField(
         choices=Frequency.choices,
         blank=True,
         help_text="The frequency at which the registry updates the publication, based on the "
@@ -178,7 +168,7 @@ class Collection(Model):
         "described below, which will be rendered as a bullet list.",
     )
 
-    last_reviewed = DateField(
+    last_reviewed = models.DateField(
         blank=True,
         null=True,
         verbose_name="last reviewed",
@@ -193,88 +183,88 @@ class Collection(Model):
         "used or additional fields, as Markdown text.",
     )
 
-    json_format = BooleanField(default=True)
-    excel_format = BooleanField(default=True)
-    csv_format = BooleanField(default=True)
+    json_format = models.BooleanField(default=True)
+    excel_format = models.BooleanField(default=True)
+    csv_format = models.BooleanField(default=True)
 
-    public = BooleanField(
+    public = models.BooleanField(
         default=False,
         help_text="If the active job's tasks completed without errors and all the fields below in "
         "all languages are filled in, check this box to make the publication visible to "
         "anonymous users. Otherwise, it is visible to administrators only.",
     )
-    frozen = BooleanField(
+    frozen = models.BooleanField(
         default=False, help_text="If the spider is broken, check this box to prevent the scheduling of new jobs."
     )
 
-    created = DateTimeField(auto_now_add=True, blank=True, null=True, db_index=True)
-    modified = DateTimeField(auto_now=True, blank=True, null=True, db_index=True)
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True, db_index=True)
+    modified = models.DateTimeField(auto_now=True, blank=True, null=True, db_index=True)
 
-    objects = Manager()
+    objects = models.Manager()
     visible = PublicCollectionManager()
 
     def __str__(self):
         return f"{self.title} ({self.id})"
 
 
-class License(Model):
+class License(models.Model):
     class Meta:
         verbose_name = "data license"
 
-    name = TextField(blank=True, help_text="The official name of the license.")
+    name = models.TextField(blank=True, help_text="The official name of the license.")
     description = MarkdownxField(
         blank=True,
         help_text="A brief description of the permissions, conditions and limitations, as " "Markdown text.",
     )
-    url = TextField(blank=True, verbose_name="URL", help_text="The canonical URL of the license.")
+    url = models.TextField(blank=True, verbose_name="URL", help_text="The canonical URL of the license.")
 
-    created = DateTimeField(auto_now_add=True, blank=True, null=True, db_index=True)
-    modified = DateTimeField(auto_now=True, blank=True, null=True, db_index=True)
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True, db_index=True)
+    modified = models.DateTimeField(auto_now=True, blank=True, null=True, db_index=True)
 
     def __str__(self):
         return f"{self.name} ({self.id})"
 
 
-class Issue(Model):
+class Issue(models.Model):
     class Meta:
         verbose_name = "quality issue"
 
     description = MarkdownxField(help_text="A one-line description of the quality issue, as Markdown text.")
-    collection = ForeignKey("Collection", related_name="issue", on_delete=CASCADE)
+    collection = models.ForeignKey("Collection", related_name="issue", on_delete=models.CASCADE)
 
-    created = DateTimeField(auto_now_add=True, blank=True, null=True, db_index=True)
-    modified = DateTimeField(auto_now=True, blank=True, null=True, db_index=True)
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True, db_index=True)
+    modified = models.DateTimeField(auto_now=True, blank=True, null=True, db_index=True)
 
 
-class Task(Model):
+class Task(models.Model):
     class Meta:
         verbose_name = "job task"
 
-    job = ForeignKey("Job", related_name="task", on_delete=CASCADE)
-    start = DateTimeField(blank=True, null=True, db_index=True)
-    end = DateTimeField(blank=True, null=True, db_index=True)
+    job = models.ForeignKey("Job", related_name="task", on_delete=models.CASCADE)
+    start = models.DateTimeField(blank=True, null=True, db_index=True)
+    end = models.DateTimeField(blank=True, null=True, db_index=True)
 
-    class Status(TextChoices):
+    class Status(models.TextChoices):
         WAITING = "WAITING", "WAITING"
         PLANNED = "PLANNED", "PLANNED"
         RUNNING = "RUNNING", "RUNNING"
         COMPLETED = "COMPLETED", "COMPLETED"
 
-    status = TextField(choices=Status.choices, blank=True)
+    status = models.TextField(choices=Status.choices, blank=True)
 
-    class Result(TextChoices):
+    class Result(models.TextChoices):
         OK = "OK", "OK"
         FAILED = "FAILED", "FAILED"
 
-    result = TextField(choices=Result.choices, blank=True)
-    note = TextField(blank=True, help_text="Metadata about any failure.")
-    context = JSONField(blank=True, default=dict)
+    result = models.TextField(choices=Result.choices, blank=True)
+    note = models.TextField(blank=True, help_text="Metadata about any failure.")
+    context = models.JSONField(blank=True, default=dict)
 
-    type = TextField(blank=True)
-    order = IntegerField(blank=True, null=True)
+    type = models.TextField(blank=True)
+    order = models.IntegerField(blank=True, null=True)
 
-    created = DateTimeField(auto_now_add=True, blank=True, null=True, db_index=True)
-    modified = DateTimeField(auto_now=True, blank=True, null=True, db_index=True)
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True, db_index=True)
+    modified = models.DateTimeField(auto_now=True, blank=True, null=True, db_index=True)
 
     def __str__(self):
         return f"#{self.id}({self.type})"
