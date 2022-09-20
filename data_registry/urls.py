0291b7a80960479a4951f9371ce87ef5c1bc7bdd
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-09-20 17:21:24 -0400

    chore: Move i18n views to separate module

--- data_registry/urls.py
+++ data_registry/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from data_registry import views
+from data_registry import i18n, views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("search/", views.search, name="search"),
     path("publication/<int:id>", views.detail, name="detail"),
     path("wipe_job/<int:job_id>", views.wipe_job, name="wipe-job"),
-    path("i18n/setlang/", views.set_language, name="set_language"),
+    path("i18n/setlang/", i18n.set_language, name="set_language"),
     # Uncomment after re-integrating Spoonbill.
     # path("excel_data/<int:job_id>/<str:job_range>", views.excel_data, name="excel_data"),
     # path("excel_data/<int:job_id>", views.excel_data, name="all_excel_data"),
