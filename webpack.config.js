377ed9c182b9fd7da9f0fed07a90a85af0638e91
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-09-21 23:24:39 -0400

    chore: Combine postcss.config.js into webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
-// https://getbootstrap.com/docs/4.6/getting-started/webpack/#importing-precompiled-sass
+// https://getbootstrap.com/docs/5.2/getting-started/webpack/#import-bootstrap
+// https://webpack.js.org/loaders/postcss-loader/#autoprefixer
 // https://webpack.js.org/loaders/sass-loader/#extracts-css-into-separate-files
 // https://github.com/webpack-contrib/mini-css-extract-plugin
 
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path');
 
 module.exports = {
@@ -15,7 +16,19 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: ['autoprefixer']
+              }
+            }
+          },
+          'sass-loader'
+        ]
       }
     ]
   },
