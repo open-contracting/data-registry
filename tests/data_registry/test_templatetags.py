cbb2dfccf552ea60fab5e039ae821f06e1e00c65
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-11-25 16:47:48 -0500

    test: Update tests to use templatetags

--- tests/data_registry/test_templatetags.py
+++ tests/data_registry/test_templatetags.py
@@ -1,13 +1,10 @@
-from unittest.mock import patch
-
 from django.test import SimpleTestCase
 
-from exporter.util import Export
+from data_registry.templatetags.registry import humanfilesize
 
 
 class UtilTests(SimpleTestCase):
     def test_human_file_size(self):
-        export = Export()
         for size, expected in (
             (1, "0.1 MB"),
             (949999, "0.9 MB"),
@@ -17,6 +14,4 @@ class UtilTests(SimpleTestCase):
             (1234567890, "1,235 MB"),
         ):
             with self.subTest(size=size):
-                with patch("os.path.getsize") as getsize:
-                    getsize.return_value = size
-                    self.assertEqual(export.human_file_size(size), expected)
+                self.assertEqual(humanfilesize(size), expected)
