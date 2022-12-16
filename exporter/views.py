b6821d50ff54ab08a735ad9f03b1b3f9a1a53aab
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-12-16 13:57:26 -0500

    fix: Content-Type and Content-Disposition's filename were incorrect for Content-Encoding: gzip

--- exporter/views.py
+++ exporter/views.py
@@ -20,22 +20,28 @@ def download_export(request):
 
     # Set Content-Encoding to skip GZipMiddleware.
     # https://docs.djangoproject.com/en/3.2/ref/middleware/#module-django.middleware.gzip
-    headers = {}
-    if suffix in ("jsonl.gz", "csv.tar.gz"):
-        headers = {"Content-Encoding": "gzip"}
+    if suffix == "jsonl.gz":
+        # https://stackoverflow.com/a/67807011/244258
+        headers = {"Content-Encoding": "gzip", "Content-Type": "application/jsonlines+json"}
+        extension = "jsonl"
+    if suffix == "csv.tar.gz":
+        # https://stackoverflow.com/a/57954148/244258
+        headers = {"Content-Encoding": "gzip", "Content-Type": "application/x-tar"}
+        extension = "csv.tar"
+    else:
+        headers = {}
+        extension = suffix
 
     if full:
-        basename = f"full.{suffix}"
-        filename = f"{spider}_full.{suffix}"
+        stem = "full"
     else:
-        # Guard against path traversal.
-        year = int(year)
-        basename = f"{year}.{suffix}"
-        filename = f"{spider}_{year}.{suffix}"
+        stem = int(year)  # guard against path traversal
 
-    export = Export(job_id, basename=basename)
+    export = Export(job_id, basename=f"{stem}.{extension}")
 
     if export.status != TaskStatus.COMPLETED:
         return HttpResponseNotFound("File not found")
 
-    return FileResponse(export.path.open("rb"), as_attachment=True, filename=filename, headers=headers)
+    return FileResponse(
+        export.path.open("rb"), as_attachment=True, filename=f"{spider}_{stem}.{extension}", headers=headers
+    )
