908a42a91cd03b6704e6482744b12accb979616e
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-12-17 18:10:47 -0500

    fix: Skip GZipMiddleware for downloads

--- exporter/views.py
+++ exporter/views.py
@@ -8,40 +8,27 @@ def download_export(request):
     """
     Returns an exported file as a FileResponse object.
     """
-    spider = request.GET.get("spider")
-    job_id = request.GET.get("job_id")
+    job_id = int(request.GET.get("job_id", 0))  # guard against path traversal
     full = request.GET.get("full")
-    year = request.GET.get("year")
+    year = int(request.GET.get("year", 0))  # guard against path traversal
     suffix = request.GET.get("suffix")
+    spider = request.GET.get("spider")
 
     # Guard against path traversal.
     if suffix not in ("jsonl.gz", "csv.tar.gz", "xlsx"):
         return HttpResponseBadRequest("Suffix not recognized")
 
-    # Set Content-Encoding to skip GZipMiddleware.
-    # https://docs.djangoproject.com/en/3.2/ref/middleware/#module-django.middleware.gzip
-    if suffix == "jsonl.gz":
-        # https://stackoverflow.com/a/67807011/244258
-        headers = {"Content-Encoding": "gzip", "Content-Type": "application/jsonlines+json"}
-        extension = "jsonl"
-    if suffix == "csv.tar.gz":
-        # https://stackoverflow.com/a/57954148/244258
-        headers = {"Content-Encoding": "gzip", "Content-Type": "application/x-tar"}
-        extension = "csv.tar"
-    else:
-        headers = {}
-        extension = suffix
-
-    if full:
-        stem = "full"
-    else:
-        stem = int(year)  # guard against path traversal
-
-    export = Export(job_id, basename=f"{stem}.{extension}")
+    stem = "full" if full else year
+    export = Export(job_id, basename=f"{stem}.{suffix}")
 
     if export.status != TaskStatus.COMPLETED:
         return HttpResponseNotFound("File not found")
 
     return FileResponse(
-        export.path.open("rb"), as_attachment=True, filename=f"{spider}_{stem}.{extension}", headers=headers
+        export.path.open("rb"),
+        as_attachment=True,
+        filename=f"{spider}_{stem}.{suffix}",
+        # Set Content-Encoding to skip GZipMiddleware. (ContentEncodingMiddleware removes the empty header.)
+        # https://docs.djangoproject.com/en/3.2/ref/middleware/#module-django.middleware.gzip
+        headers={"Content-Encoding": ""},
     )
