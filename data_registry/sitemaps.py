0a5bfb11bd7fe7b377175c21c6f89ed3eda3fb72
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-12-11 17:59:15 -0500

    feat: Add sitemap.xml

---
+++ data_registry/sitemaps.py
@@ -0,0 +1,32 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+from data_registry import models
+
+
+class StaticViewSitemap(Sitemap):
+    i18n = True
+    alternates = True
+    protocol = "https"
+
+    def items(self):
+        return ["index", "search"]
+
+    def location(self, item):
+        return reverse(item)
+
+
+class CollectionSitemap(Sitemap):
+    i18n = True
+    alternates = True
+    protocol = "https"
+
+    # See data_registry.util.collection_queryset().
+    def items(self):
+        return models.Collection.objects.visible()
+
+    def location(self, item):
+        return reverse("detail", kwargs={"id": item.pk})
+
+    def lastmod(self, obj):
+        return obj.modified
--- data_registry/urls.py
+++ data_registry/urls.py
@@ -1,15 +1,22 @@
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path
 
-from data_registry import i18n, views
+from data_registry import i18n, sitemaps, views
+
+maps = {
+    "static": sitemaps.StaticViewSitemap,
+    "detail": sitemaps.CollectionSitemap,
+}
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("search/", views.search, name="search"),
     path("publication/<int:id>", views.detail, name="detail"),
     path("publication/<int:id>/download", views.download_export, name="download"),
-    # https://code.djangoproject.com/ticket/26556
-    path("i18n/setlang/", i18n.set_language, name="set-language"),
     # Uncomment after re-integrating Spoonbill.
     # path("excel-data/<int:job_id>/<str:job_range>", views.excel_data, name="excel-data"),
     # path("excel-data/<int:job_id>", views.excel_data, name="all-excel-data"),
+    # https://code.djangoproject.com/ticket/26556
+    path("i18n/setlang/", i18n.set_language, name="set-language"),
+    path("sitemap.xml", sitemap, {"sitemaps": maps}, name="django.contrib.sitemaps.views.sitemap"),
 ]
