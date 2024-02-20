65fcbd35e8e35638a9c129ea27e602d15aa14180
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2024-02-20 18:02:23 -0300

    tests: add more test cases for publications_api

--- tests/data_registry/test_views.py
+++ tests/data_registry/test_views.py
@@ -20,11 +20,6 @@ class ViewsTests(TestCase):
             active=True,
         )
 
-        Collection.objects.create(
-            title="Non public collection",
-            public=False,
-        )
-
     @patch("exporter.util.Export.get_files")
     def test_detail(self, get_files):
         get_files.return_value = {"jsonl": {"by_year": [{"year": 2022, "size": 1}]}}
@@ -37,7 +32,6 @@ class ViewsTests(TestCase):
         self.assertContains(response, f"""<a href="{url}" rel="nofollow" download>2022</a>""", html=True)
 
     def test_publications_api(self):
-        response = Client().get("/en/publications.json")
         expected_response = {
             "id": self.collection.id,
             "title": self.collection.title_en,
@@ -53,9 +47,23 @@ class ViewsTests(TestCase):
             "source_url": "",
             "region": "",
         }
+        response = Client().get("/en/publications.json")
         self.assertEqual(response.status_code, 200)
-        self.assertJSONEqual(str(response.content, encoding="utf8"), [expected_response])
-
+        self.assertEqual(response.json(), [expected_response])
         response = Client().get("/es/publications.json")
+        self.assertEqual(response.status_code, 200)
         expected_response["title"] = self.collection.title_es
-        self.assertJSONEqual(str(response.content, encoding="utf8"), [expected_response])
+        self.assertEqual(response.json(), [expected_response])
+        self.collection.delete()
+        self.job.delete()
+
+        for public, active_job in ((False, True), (True, False), (False, False)):
+            with self.subTest(public=public, active_job=active_job):
+                collection = Collection.objects.create(title="Test", public=public)
+                if active_job:
+                    collection.job.create(
+                        active=True,
+                    )
+                response = Client().get("/es/publications.json")
+                self.assertEqual(response.status_code, 200)
+                self.assertEqual(response.json(), [])
