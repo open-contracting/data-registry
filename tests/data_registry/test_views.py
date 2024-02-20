b9e3952b4ee944e0f2805e3643a91bcd0b45abaf
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2024-02-19 21:09:17 -0300

    tests: test publications_api

--- tests/data_registry/test_views.py
+++ tests/data_registry/test_views.py
@@ -11,11 +11,18 @@ class ViewsTests(TestCase):
     def setUpTestData(cls):
         cls.collection = Collection.objects.create(
             title="Dirección Nacional de Contrataciones Públicas (DNCP)",
+            title_en="National Directorate of Public Procurement (DNCP)",
+            title_es="Dirección Nacional de Contrataciones Públicas (DNCP)",
             source_id="paraguay_dncp_records",
             public=True,
         )
         cls.job = cls.collection.job.create(
             active=True,
+        )
+
+        Collection.objects.create(
+            title="Non public collection",
+            public=False,
         )
 
     @patch("exporter.util.Export.get_files")
@@ -28,3 +35,27 @@ class ViewsTests(TestCase):
 
         self.assertTemplateUsed("detail.html")
         self.assertContains(response, f"""<a href="{url}" rel="nofollow" download>2022</a>""", html=True)
+
+    def test_publications_api(self):
+        response = Client().get("/en/publications.json")
+        expected_response = {
+            "id": self.collection.id,
+            "title": self.collection.title_en,
+            "source_id": self.collection.source_id,
+            "update_frequency": Collection.UpdateFrequency.UNKNOWN.name,
+            "country": "",
+            "data_to": None,
+            "data_from": None,
+            "frozen": False,
+            "language": "",
+            "last_retrieved": None,
+            "retrieval_frequency": "",
+            "source_url": "",
+            "region": "",
+        }
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(str(response.content, encoding="utf8"), [expected_response])
+
+        response = Client().get("/es/publications.json")
+        expected_response["title"] = self.collection.title_es
+        self.assertJSONEqual(str(response.content, encoding="utf8"), [expected_response])
