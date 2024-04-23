9fb35871f1d070468dcfdfd68c059239eefc6e56
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2024-04-23 15:19:11 -0400

    fix: update tests

--- tests/data_registry/process_manager/test_process.py
+++ tests/data_registry/process_manager/test_process.py
@@ -24,14 +24,9 @@ class ProcessTests(TransactionTestCase):
     def test(self):
         collection = Collection.objects.get(pk=1)
 
-        with patch("data_registry.process_manager.process.get_runner") as mock_get_runner, patch(
-            "data_registry.process_manager.process.update_collection_availability"
-        ) as mock_update_collection_availability:
+        with patch("data_registry.process_manager.process.get_runner") as mock_get_runner:
             # get_runner returns only TestTask
             mock_get_runner.return_value = TestTask()
-            # skip update_collection_availability (does nothing, counts are not set!)
-            mock_update_collection_availability.return_value = None
-            # skip update_collection_metadaat (does nothing, metadata are not set!)
 
             settings.JOB_TASKS_PLAN = ["test"]
 
