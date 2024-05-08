1e67eda85843721661e280575dbb631176b0ee87
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-05-08 01:12:42 -0400

    feat: Remove lockfiles when running Exporter and Flattener tasks, allowing tasks to restart, closes #354

--- data_registry/process_manager/task/exporter.py
+++ data_registry/process_manager/task/exporter.py
@@ -5,12 +5,18 @@ from exporter.util import Export, publish
 class Exporter(TaskManager):
     final_output = True
 
+    def get_export(self):
+        return Export(self.job.id, basename="full.jsonl.gz")
+
     def run(self):
+        self.get_export().unlock()
+
         publish({"job_id": self.job.id, "collection_id": self.job.context["process_id_pelican"]}, "exporter_init")
 
     def get_status(self):
-        status = Export(self.job.id, basename="full.jsonl.gz").status
-        return exporter_status_to_task_status(status)
+        export = self.get_export()
+
+        return exporter_status_to_task_status(export.status)
 
     @skip_if_not_started
     def wipe(self):
