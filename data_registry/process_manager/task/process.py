open-contracting/data-registry

commit 15d805997b8fc285da7a09d487a2cb1444dd873e

fix: Add exception for nigeria_ebonyi_state

data_registry/process_manager/task/process.py
@@ -16,6 +16,10 @@ def url_for_collection(*parts):
 
 def parse_date(dt):
     if dt:
+        # nigeria_ebonyi_state
+        if dt.count(":") == 3:
+            dt = ".".join(dt.rsplit(":", 1))
+
         if len(dt) == 10:
             return datetime.strptime(dt, "%Y-%m-%d").date()
         elif "." in dt:

commit f4400e2033fae335aaec3adf64ce5f8d94277fe0

fix: Fix regression in 15d8059 which broke date parsing for time zone offsets

data_registry/process_manager/task/process.py
@@ -17,7 +17,7 @@ def url_for_collection(*parts):
 def parse_date(dt):
     if dt:
         # nigeria_ebonyi_state
-        if dt.count(":") == 3:
+        if dt.endswith("Z") and dt.count(":") == 3:
             dt = ".".join(dt.rsplit(":", 1))
 
         if len(dt) == 10: