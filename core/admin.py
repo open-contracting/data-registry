fa019c18e6a10b45fd0922af1c2e3aab65390584
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-12-03 14:43:07 -0500

    fix: app_label is optional. Change was in Django 4.1. https://docs.djangoproject.com/en/5.1/releases/4.1/#django-contrib-admin

--- core/admin.py
+++ core/admin.py
@@ -1,29 +1,29 @@
 from django.contrib import admin
 
+ORDER = [
+    "Collection",
+    "License",
+    "Job",
+    "Task",
+]
+
+
+def _key(model):
+    try:
+        return str(ORDER.index(model["object_name"]))
+    except ValueError:
+        return model["name"]
+
 
 class CoreAdminSite(admin.AdminSite):
     site_header = "Data registry administration"
     site_title = "Data registry administration"
 
-    def get_app_list(self, request, app_label):
+    def get_app_list(self, request, app_label=None):
         """Sorts the data registry's models in a logical order."""
-        order = [
-            "Collection",
-            "License",
-            "Job",
-            "Task",
-        ]
+        app_list = super().get_app_list(request, app_label=app_label)
 
-        def sort(model):
-            try:
-                return str(order.index(model["object_name"]))
-            except ValueError:
-                return model["name"]
-
-        app_list = super().get_app_list(request)
-
-        if app_list:
-            app = next(app for app in app_list if app["app_label"] == "data_registry")
-            app["models"].sort(key=sort)
+        if app := next((app for app in app_list if app["app_label"] == "data_registry"), None):
+            app["models"].sort(key=_key)
 
         return app_list
