3553f8c8d0439328e7dc450d80d64f4f15280bf7
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2025-06-27 14:38:44 -0300

    feat: add coverage command

--- data_registry/process_manager/task/coverage.py
+++ data_registry/process_manager/task/coverage.py
@@ -1,13 +1,6 @@
-import gzip
-import shutil
-import tempfile
-from pathlib import Path
-
-import ocdscardinal
-
 from data_registry import models
 from data_registry.process_manager.util import TaskManager, skip_if_not_started
-from exporter.util import Export
+from exporter.util import Export, publish
 
 
 def filter_json_paths_by_suffix(json_paths, suffix):
@@ -21,41 +14,8 @@ class Coverage(TaskManager):
         return Export(self.job.pk, basename="coverage")
 
     def run(self):
-        export = self.get_export()
-
-        export.lock()
-
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            infile = Path(tmpdirname) / "coverage.jsonl"
-
-            with gzip.open(Export(self.job.pk, "full.jsonl.gz").path) as i, infile.open("wb") as o:
-                shutil.copyfileobj(i, o)
-
-            coverage = ocdscardinal.coverage(str(infile))
-
-            attributes_mapping = {
-                "tenders_count": ("/tender/",),
-                "tenderers_count": ("/tender/tenderers[]",),
-                "tenders_items_count": ("/tender/items[]",),
-                "parties_count": ("/parties[]",),
-                "awards_count": ("/awards[]",),
-                "awards_items_count": ("/awards[]/items[]",),
-                "awards_suppliers_count": ("/awards[]/suppliers[]",),
-                "contracts_count": ("/contracts[]",),
-                "contracts_items_count": ("/contracts[]/items[]",),
-                "contracts_transactions_count": ("/contracts[]/transactions[]",),
-                "documents_count": filter_json_paths_by_suffix(coverage, "documents[]"),
-                "plannings_count": ("/planning/",),
-                "milestones_count": filter_json_paths_by_suffix(coverage, "milestones[]"),
-                "amendments_count": filter_json_paths_by_suffix(coverage, "amendments[]"),
-            }
-            for attribute, json_paths in attributes_mapping.items():
-                setattr(self.job, attribute, sum(coverage.get(json_path, 0) for json_path in json_paths))
-
-            self.job.coverage = coverage
-            self.job.save(update_fields=["modified", "coverage", *attributes_mapping])
-
-        export.unlock()
+        self.get_export().unlock()
+        publish({"job_id": self.job.pk}, "coverage_init")
 
     def get_status(self):
         if self.get_export().locked:
---
+++ exporter/management/commands/coverage.py
@@ -0,0 +1,68 @@
+import gzip
+import shutil
+import tempfile
+from pathlib import Path
+
+from click import BaseCommand
+from ocdscardinal import ocdscardinal
+from yapw.methods import ack
+
+from data_registry.models import Job
+from exporter.util import Export, consume, decorator
+
+
+class Command(BaseCommand):
+    help = """
+    Get the coverage of exported full JSONL compiled collections using Cardinal.
+    """
+
+    def handle(self, *args, **options):
+        consume(on_message_callback=callback, queue="coverage_init", decorator=decorator)
+
+
+def filter_json_paths_by_suffix(json_paths, suffix):
+    return [json_path for json_path in json_paths if json_path.endswith(suffix)]
+
+
+def callback(state, channel, method, properties, input_message):
+    job_id = input_message.get("job_id")
+    job = Job.objects.get(id=job_id)
+
+    export = Export(job_id, basename="coverage")
+
+    export.lock()
+
+    # Acknowledge now to avoid connection losses. The rest can run for hours and is irreversible anyhow.
+    ack(state, channel, method.delivery_tag)
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        infile = Path(tmpdirname) / "coverage.jsonl"
+
+        with gzip.open(Export(job_id, "full.jsonl.gz").path) as i, infile.open("wb") as o:
+            shutil.copyfileobj(i, o)
+
+        coverage = ocdscardinal.coverage(str(infile))
+
+        attributes_mapping = {
+            "tenders_count": ("/tender/",),
+            "tenderers_count": ("/tender/tenderers[]",),
+            "tenders_items_count": ("/tender/items[]",),
+            "parties_count": ("/parties[]",),
+            "awards_count": ("/awards[]",),
+            "awards_items_count": ("/awards[]/items[]",),
+            "awards_suppliers_count": ("/awards[]/suppliers[]",),
+            "contracts_count": ("/contracts[]",),
+            "contracts_items_count": ("/contracts[]/items[]",),
+            "contracts_transactions_count": ("/contracts[]/transactions[]",),
+            "documents_count": filter_json_paths_by_suffix(coverage, "documents[]"),
+            "plannings_count": ("/planning/",),
+            "milestones_count": filter_json_paths_by_suffix(coverage, "milestones[]"),
+            "amendments_count": filter_json_paths_by_suffix(coverage, "amendments[]"),
+        }
+        for attribute, json_paths in attributes_mapping.items():
+            setattr(job, attribute, sum(coverage.get(json_path, 0) for json_path in json_paths))
+
+        job.coverage = coverage
+        job.save(update_fields=["modified", "coverage", *attributes_mapping])
+
+    export.unlock()
