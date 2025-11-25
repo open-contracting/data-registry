a5aab90cc21b3d16842c2416ea2351ce621efadb
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2025-11-25 16:48:09 -0300

    feat: use spoonbill-like table options (#433)
    * feat: use spoonbill-like table options
    * fix(flattener): call flatterer_flatten recursively
    * i18n: fixes
    * fix: Add csv and threads back to flatterer_flatten call. Remove single-use variable.
    * i18n: Remove fuzzy translations
    * fix: remove dummy flattener schema use
    
    ---------
    
    Co-authored-by: James McKinney <26463+jpmckinney@users.noreply.github.com>

--- exporter/management/commands/flattener.py
+++ exporter/management/commands/flattener.py
@@ -124,7 +124,19 @@ def flatterer_flatten(file_path, infile, outdir, *, csv=False, xlsx=False, threa
     -  Otherwise (``csv=True``), re-raise the error.
     """
     try:
-        return flatterer.flatten(infile, outdir, csv=csv, xlsx=xlsx, ndjson=True, force=True, threads=threads)
+        return flatterer.flatten(
+            infile,
+            outdir,
+            # https://docs.flatterer.dev/options.html
+            csv=csv,
+            xlsx=xlsx,
+            threads=threads,
+            ndjson=True,  # the input is newline-delimited JSON
+            force=True,  # delete the temporary "flatten" directory, if it exists
+            pushdown=["ocid", "id"],  # copy fields to child tables as {parent}_ocid and {parent}_id
+            no_link=True,  # do not create _link and _link_{parent} (instead use the pushdown fields)
+            inline_one_to_one=True,  # inline arrays that always contain at most one entry
+        )
     except RuntimeError as e:
         if not xlsx:  # CSV-only should succeed.
             raise
